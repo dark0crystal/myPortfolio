a20fa3c9c2aa750a035bdba00894d4428d789131
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-14 20:35:47 +0400

    height fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="absolute inset-0 -z-10 h-full w-full px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
 
       {/* Social Media Links */}
-      <div className="flex justify-center items-center w-screen gap-6 h-fit">
+      <div className="flex justify-center items-center w-screen gap-6 ">
         {/* X (Twitter) */}
         <a
           href="https://x.com/_5lo_9"
