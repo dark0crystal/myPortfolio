7f27be26717d612f0e55b82cfe6022a30a54db57
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-22 15:02:41 +0400

    icon import fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import icon from "./icon.jpeg"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/icon.jpeg" type="image/jpeg" />
+        <link rel="icon" href="./icon.jpeg" type="image/jpeg" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
