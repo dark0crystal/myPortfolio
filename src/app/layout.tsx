2d48dd0f5773947dba1669e07989b08ebaf0bf9f
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-22 15:03:01 +0400

    f

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import icon from "./icon.jpeg"
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
